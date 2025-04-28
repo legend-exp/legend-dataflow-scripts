b195a5e8cfc42ee303c7042a088d1549e62c5b55
George Marshall <george.marshall.20@ucl.ac.uk>
2025-04-28 11:54:50 -0700

    Lgdo update (#123)
    * update to new lgdo
    
    * add file info to table
    
    * add single channel variants
    
    * split util paths into own file
    
    * roll back default changes
    
    * dpz changes
    
    * update tests
    
    * small plotting and bug fixes
    
    * tau to pz and integrate dpz
    
    * move build chanlist/filelist to scripts
    
    * add alias table
    
    * add configs and meta explicitly to rules, add alias table and table_map to tiers
    
    * cleanup and add compression to orca
    
    * add multiprocess options and update numba cache
    
    * update pkg versions and pz rule
    
    * add multiprocessing
    
    * period and run to ints
    
    * cleanup
    
    * update pkgs
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ build_chanlist.py
@@ -0,0 +1,83 @@
+# ruff: noqa: F821, T201
+
+from pathlib import Path
+
+from dbetto import TextDB
+
+from legenddataflow.FileKey import ChannelProcKey
+from legenddataflow.patterns import (
+    get_pattern_pars_tmp_channel,
+    get_pattern_plts_tmp_channel,
+)
+
+
+def get_chanlist(timestamp, datatype, det_status, channelmap, system):
+    if isinstance(det_status, (str, Path)):
+        det_status = TextDB(det_status, lazy=True)
+
+    if isinstance(channelmap, (str, Path)):
+        channelmap = TextDB(channelmap, lazy=True)
+
+    if isinstance(det_status, TextDB):
+        status_map = det_status.statuses.on(timestamp, system=datatype)
+    else:
+        status_map = det_status.valid_for(timestamp, system=datatype)
+    if isinstance(channelmap, TextDB):
+        chmap = channelmap.channelmaps.on(timestamp, system=datatype)
+    else:
+        chmap = channelmap.valid_for(timestamp, system=datatype)
+
+    # only restrict to a certain system (geds, spms, ...)
+    channels = []
+    for channel in chmap.map("system", unique=False)[system].map("name"):
+        if channel not in status_map:
+            msg = f"{channel} is not found in the status map (on {timestamp})"
+            raise RuntimeError(msg)
+        if status_map[channel].processable is False:
+            continue
+        channels.append(channel)
+
+    if len(channels) == 0:
+        print("WARNING: No channels found")
+
+    return channels
+
+
+def get_par_chanlist(
+    setup,
+    keypart,
+    tier,
+    det_status,
+    chan_maps,
+    system,
+    datatype="cal",
+    name=None,
+    extension="yaml",
+):
+    key = ChannelProcKey.parse_keypart(keypart)
+
+    chan_list = get_chanlist(key.timestamp, key.datatype, det_status, chan_maps, system)
+
+    par_pattern = get_pattern_pars_tmp_channel(
+        setup, tier, name, datatype=datatype, extension=extension
+    )
+
+    return ChannelProcKey.get_channel_files(keypart, par_pattern, chan_list)
+
+
+def get_plt_chanlist(
+    setup,
+    keypart,
+    tier,
+    det_status,
+    chan_maps,
+    system,
+    name=None,
+):
+    key = ChannelProcKey.parse_keypart(keypart)
+
+    chan_list = get_chanlist(key.timestamp, key.datatype, det_status, chan_maps, system)
+
+    par_pattern = get_pattern_plts_tmp_channel(setup, tier, name)
+
+    return ChannelProcKey.get_channel_files(keypart, par_pattern, chan_list)
