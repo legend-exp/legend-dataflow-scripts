5f8b48cdaa313773f453647876df3dbc26bc7585
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-05-19 12:23:18 +0200

    fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 sys.path.insert(0, Path(__file__).parents[2].resolve().as_posix())
 
-project = "legend-dataflow"
-copyright = "2024, the LEGEND Collaboration"
+project = "legend-dataflow-scripts"
+copyright = "2025, the LEGEND Collaboration"
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -28,7 +28,7 @@ master_doc = "index"
 # Furo theme
 html_theme = "furo"
 html_theme_options = {
-    "source_repository": "https://github.com/legend-exp/legend-dataflow",
+    "source_repository": "https://github.com/legend-exp/legend-dataflow-scripts",
     "source_branch": "main",
     "source_directory": "docs/source",
 }
