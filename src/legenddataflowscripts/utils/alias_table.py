e4e9150d3521d43434f65b29fac10f6b34b9c005
ggmarshall <george.marshall.20@ucl.ac.uk>
2025-08-01 07:41:02 +0200

    add convert parents to structs

--- src/legenddataflowscripts/utils/alias_table.py
+++ src/legenddataflowscripts/utils/alias_table.py
@@ -6,6 +6,29 @@ from pathlib import Path
 import h5py
 
 
+def convert_parents_to_structs(h5group):
+    if h5group.parent.name != "/" and len(h5group.parent.attrs) == 0:
+        h5group.parent.attrs.update(
+            {"datatype": "struct{" + h5group.name.split("/")[-1] + "}"}
+        )
+    elif (
+        len(h5group.parent.attrs) > 0
+        and h5group.name.split("/")[-1] not in h5group.parent.attrs["datatype"]
+    ):
+        h5group.parent.attrs.update(
+            {
+                "datatype": h5group.parent.attrs["datatype"][:-1]
+                + ","
+                + h5group.name.split("/")[-1]
+                + "}"
+            }
+        )
+    else:
+        return
+    convert_parents_to_structs(h5group.parent)
+    return
+
+
 def alias_table(file: str | Path, mapping: str):
     """
     Create an alias table for the given file and mapping.
@@ -26,5 +49,7 @@ def alias_table(file: str | Path, mapping: str):
                 if isinstance(alias, list | tuple):
                     for a in alias:
                         f[a] = f[raw_id]
+                        convert_parents_to_structs(f[a])
                 else:
                     f[alias] = f[raw_id]
+                    convert_parents_to_structs(f[alias])
